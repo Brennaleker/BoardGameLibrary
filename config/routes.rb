ee8b3b2bfab38b22687cc4ec4434c486c14bc842
Brennaleker <brennaleker@gmail.com>
2015-09-07 15:40:59 -0700

    migration and model validation

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,3 @@ Rails.application.routes.draw do
   resources :board_games, except:[:index]
   get '/', to: 'board_games#index', as: 'home'
 end
-
-//testing 
--- db/migrate/20150904215501_create_board_games.rb
+++ db/migrate/20150904215501_create_board_games.rb
@@ -3,6 +3,14 @@ class CreateBoardGames < ActiveRecord::Migration
     create_table :board_games do |t|
 
       t.timestamps null: false
+      t.string :title
+      t.string :creator
+      t.text :description
+      t.integer :min_players
+      t.integer :max_players
+      t.integer :bgg_score
+      t.integer :min_time
+      t.integer :max_time
     end
   end
 end
