beda7d9d86859d11d8bf38f6a6fde697745247fa
Brennaleker <brennaleker@gmail.com>
2015-09-07 22:18:17 -0700

    basic seed data and restructured migration

--- db/migrate/20150904215501_create_board_games.rb
+++ db/migrate/20150904215501_create_board_games.rb
@@ -5,12 +5,16 @@ class CreateBoardGames < ActiveRecord::Migration
       t.timestamps null: false
       t.string :title
       t.string :creator
+      t.string :image
       t.text :description
       t.integer :min_players
       t.integer :max_players
-      t.integer :bgg_score
       t.integer :min_time
       t.integer :max_time
+      t.integer :bgg_score
+      t.boolean :expansion
+      t.string :expansion_to
+
     end
   end
 end
